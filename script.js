d4200ce9a17d0de56a4c8462636bd50cf2d4e98c
hunter <hskempton@gmail.com>
2017-04-26 10:12:28 -0400

    added updated JS and added classes and ids to the cars

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 $(function () {
   //intialize empty array list that will hold our seat user objects
   var listArr = [];
+  var seatNumber = '';
+  var found = false;
 
-  //triggers the form and gets information from seat click
-  $('div .seat').on(click(function (event) {
-    var seatNumber = event.key;
-    bringForm();
-  })
-
-  //function to add objects to out listArr when form is submitted
-  function addUser (seatNumber) {
-    var userName = $('#name').val();
-    var userEmail = $('#email').val();
-    var newObj = {seatN: seatNumber, name: userName, email: userEmail, reserved: true};
-    listArr.push(newObj);
+  function findIfReserved (seatNPar) {
+    for(var i = 0; i < listArr.length; i++) {
+      if (listArr[i].seatN === seatNPar) {
+        found = true;
+        break;
+      }
+    }
   }
 
-  //TODO bring form function to display and slide down form
-  //TODO display alert that confirms the reservation
+    $(".seat").on('click', function() {
+      seatNumber = $(this).attr('id');
+      findIfReserved(seatNumber);
+      if (!found) {
+      bringForm(seatNumber);
+    }
+    found = false;
+    });
+
+    function bringForm (seatNumber) {
+      $('#form').css('display','block');
+      $('#formMsg').text("You have selected seat " + seatNumber);
+    }
+
+    //function to add objects to out listArr when form is submitted
+    $('#reserveButton').on('click', function() {
+      var userName = $('#name').val();
+      var userEmail = $('#email').val();
+      var newObj = {seatN: seatNumber, name: userName, email: userEmail, reserved: true};
+      listArr.push(newObj);
+      console.log(newObj);
+      alert("You have reserved seat " + seatNumber);
+      cancel();
+
+    });
+
+    $("#cancel").on('click', function() {
+      cancel();
+    });
+
+    function cancel () {
+      $('#form').css('display','none');
+    }
 
 
 
 
 
-});
+
+
+
+  });
